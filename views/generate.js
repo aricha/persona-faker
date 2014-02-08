b58ae7eac9c15b2eceedf0da0b440dc6f4f3a3a2
Charles Chong <CC.Charles.Chong@gmail.com>
2014-02-07 16:26:00 -0800

    Added function calls to handle and respond to requests

--- views/generate.js
+++ views/generate.js
@@ -1,7 +1,11 @@
+var assertion = require('../lib/assertion.js');
+
 module.exports = function(server) {
     server.get({
         url: '/generate'
     }, function (req, res) {
-        
+        res.json(200, {
+                assertion: assertion.generateAssertion(req.params.email)
+            });
     });
 };
--- views/verify.js
+++ views/verify.js
@@ -1,7 +1,22 @@
+var assertion = require('../lib/assertion.js');
+
 module.exports = function(server) {
     server.post({
         url: '/verify'
     }, function (req, res) {
-        
+        var email = assertion.verifyAssertion(req.params.assertion);
+        if (email) {
+            res.json(200, {
+                status: 'okay',
+                email: email,
+                audience: req.params.audience,
+                expires: new Date(2100, 12),
+                issuer: "localhost"
+            });
+        } else {
+            res.json(500, {
+                status: 'failure'
+            });
+        }
     });
 };
